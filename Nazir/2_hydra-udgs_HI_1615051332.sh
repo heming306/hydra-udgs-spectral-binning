d8c9c58ccf29f4fe388a66e0de6a4330a1185819
heming306 <20660457+heming306@users.noreply.github.com>
2022-04-13 13:25:23 +0200

    Update 2_hydra-udgs_HI_1615051332.sh

--- Nazir/2_hydra-udgs_HI_1615051332.sh
+++ Nazir/2_hydra-udgs_HI_1615051332.sh
@@ -2,14 +2,14 @@
 #SBATCH --job-name='Hydra-HI' #updated name
 #SBATCH --time=72:00:00
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=14
+#SBATCH --cpus-per-task=32
 #SBATCH --mem=100GB
 #SBATCH --output=2_hydra-udgs_HI_1615051332-%j-stdout.log ##updated names
 #SBATCH --error=2_hydra-udgs_HI_1615051332-%j-stderr.log ## updated names
 
 #use last four digits of mosaic id for job-name
 
-cd /idia/projects/hydra-udgs/Nazir/hydra-udgs-HI-8channel-bin ## updated naming
+cd /idia/projects/hydra-udgs/Nazir/hydra-udgs-HI-8channel-bin2 ## updated naming
 venv=/idia/projects/hydra-udgs/CARACal_all/CARACal-venv-18Oct2021
 source $venv/bin/activate
 caracal -c 2_hydra-udgs_HI_1615051332.yml --container-tech singularity -sid /software/astro/caracal/STIMELA_IMAGES_1.7.0
