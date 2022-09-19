08d66d4ee8cc07a555c423053f687cd81f4b1631
heming306 <20660457+heming306@users.noreply.github.com>
2022-09-19 14:56:19 +0200

    Update 1_hydra-udgs-crosscal_1615051332.sh

--- 32k-reduction/1_hydra-udgs-crosscal_1615051332.sh
+++ 32k-reduction/1_hydra-udgs-crosscal_1615051332.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
-#SBATCH --job-name='Hydra-HI' #updated name
-#SBATCH --time=2-12:00:00
-#SBATCH --ntasks=1
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=30
 #SBATCH --mem=230GB
+#SBATCH --job-name='Hydra-HI' #updated name
+#SBATCH --distribution=plane=1
 #SBATCH --output=1_hydra-udgs_crosscal_1615051332-%j-stdout.log ##updated names
 #SBATCH --error=1_hydra-udgs_crosscal_1615051332-%j-stderr.log ## updated names
+#SBATCH --partition=Main
+#SBATCH --time="2-12:00:00"
+#SBATCH --mail-user="mkdnaz002@myuct.ac.za"
+#SBATCH --mail-type="START,END,FAIL"
 
 #use last four digits of mosaic id for job-name
 
