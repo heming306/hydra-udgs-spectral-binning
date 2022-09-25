930a7064900598785585acb8d091a35ec70ae421
heming306 <20660457+heming306@users.noreply.github.com>
2022-09-25 23:49:26 +0200

    Update 3_hydra-udgs-selfcal.sh

--- 32k-reduction/3_hydra-udgs-selfcal.sh
+++ 32k-reduction/3_hydra-udgs-selfcal.sh
@@ -5,8 +5,8 @@
 #SBATCH --mem=230GB
 #SBATCH --job-name='Hydra-HI' #updated name
 #SBATCH --distribution=plane=1
-#SBATCH --output=1_hydra-udgs_selfcal-%j-stdout.log ##updated names
-#SBATCH --error=1_hydra-udgs_selfcal-%j-stderr.log ## updated names
+#SBATCH --output=3_hydra-udgs_selfcal-%j-stdout.log ##updated names
+#SBATCH --error=3_hydra-udgs_selfcal-%j-stderr.log ## updated names
 #SBATCH --partition=Main
 #SBATCH --time="2-12:00:00"
 #SBATCH --mail-user="mkdnaz002@myuct.ac.za"
@@ -15,7 +15,7 @@
 #use last four digits of mosaic id for job-name
 
 cd /scratch3/users/nazir/hydra-udgs/ ## updated naming
-venv=/scratch3/users/nazir/hydra-udgs/CARACal/CARACal-venv-18Aug2022
+venv=/scratch3/users/nazir/hydra-udgs/CARACal_all/CARACal-venv-19Sep2022
 source $venv/bin/activate
-caracal -c 3_hydra-udgs-selfcal.yml --container-tech singularity -sid /software/astro/caracal/STIMELA_IMAGES_1.7.0
+caracal -c 3_hydra-udgs-selfcal.yml --container-tech singularity -sid /software/astro/caracal/STIMELA_IMAGES_1.7.5
 deactivate #closing caracal
