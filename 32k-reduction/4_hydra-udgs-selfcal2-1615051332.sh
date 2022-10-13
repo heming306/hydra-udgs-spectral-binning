8e4a2800755b55bb0713a2504acaaead16c74a26
heming306 <20660457+heming306@users.noreply.github.com>
2022-10-13 13:08:17 +0200

    Update and rename 4_hydra-udgs-selfcal2.sh to 4_hydra-udgs-selfcal2-1615051332.sh

--- 32k-reduction/4_hydra-udgs-selfcal2-1615051332.sh
+++ 32k-reduction/4_hydra-udgs-selfcal2-1615051332.sh
@@ -8,7 +8,7 @@
 #SBATCH --output=4_hydra-udgs_selfcal2-%j-stdout.log ##updated names
 #SBATCH --error=4_hydra-udgs_selfcal2-%j-stderr.log ## updated names
 #SBATCH --partition=Main
-#SBATCH --time="14-00:00:00"
+#SBATCH --time="2-00:00:00"
 #SBATCH --mail-user="mkdnaz002@myuct.ac.za"
 #SBATCH --mail-type="START,END,FAIL"
 
@@ -17,5 +17,5 @@
 cd /scratch3/users/nazir/hydra-udgs/ ## updated naming
 venv=/scratch3/users/nazir/hydra-udgs/CARACal_all/CARACal-venv-19Sep2022
 source $venv/bin/activate
-caracal -c 4_hydra-udgs-selfcal2.yml --container-tech singularity -sid /software/astro/caracal/STIMELA_IMAGES_1.7.5
+caracal -c 4_hydra-udgs-selfcal2-1615051332.yml --container-tech singularity -sid /software/astro/caracal/STIMELA_IMAGES_1.7.5
 deactivate #closing caracal
